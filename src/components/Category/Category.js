b0d30da4913dd5f3f2e05c4320b1a6c770d02fa1
hasansharif819 <hs.sharif819@gmail.com>
2022-10-27 23:43:56 +0600

    sass

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import vector from '../../resources/images/Vector.png';
-import img25 from '../../resources/images/image 25.png';
+import card1 from '../../resources/images/card1.png';
+import card2 from '../../resources/images/card2.png';
+import card3 from '../../resources/images/card3.png';
+import card4 from '../../resources/images/card4.png';
+// import vector from '../../resources/images/Vector.png';
+// import img25 from '../../resources/images/image 25.png';
 
 const Category = () => {
     return (
@@ -11,22 +15,22 @@ const Category = () => {
             </div>
             <div className='cardDiv'>
                 <div className="card">
-                    <img src={vector} alt="" />
+                    <img src={card1} alt="" />
                     <h2>Calculated Weather</h2>
                     <p>Built Wicket longer admire do barton vanity itself do in it.</p>
                 </div>
                 <div className="card2">
-                    <img src="" alt="" />
+                    <img src={card2} alt="" />
                     <h2>Best Flights</h2>
                     <p>Engrossed listening. Park gate sell they west hard for the.</p>
                 </div>
                 <div className="card">
-                    <img src={img25} alt="" />
+                    <img src={card3} alt="" />
                     <h2>Local Events</h2>
                     <p>Barton vanity itself do in it. Preferd to men it engrossed listening.</p>
                 </div>
                 <div className="card">
-                    <img src="" alt="" />
+                    <img src={card4} alt="" />
                     <h2>Customization</h2>
                     <p>We deliver outsourced aviation services for military customers</p>
                 </div>
